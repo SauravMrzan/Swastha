bf624836782faa6eef8a6a9d365056d30ac25ccc
Saurav <maharjansaurav2002@gmail.com>
2025-02-28 00:07:39 +0545

    Removing unwanted files

--- SwasthaBackEnd/routes/clinicRoute.js
+++
@@ -1,9 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-
-// const clinicController = require('../controller/clinicController');
-
-// router.post("/login", clinicController.loginClinic);
-// router.post("/register",clinicController.registerClinic);
-
-// module.exports = router;
